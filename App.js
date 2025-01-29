0dc3985d60b06b968af2b0617d04a367f555ab68
talk2dennis <adigwedennis@gmail.com>
2025-01-29 21:04:18 +0100

    learning animation

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 
-export default function App() {
+const App = () => {
+  const opacity = useSharedValue(1);
+  const scale = useSharedValue(1);
+  const rotate = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+      transform: [{ scale: scale.value }],
+      transform: [{ rotate: `${rotate.value}deg` }],
+    };
+  });
+
+  // Start animation
+  const startAnimation = () => {
+    opacity.value = withSpring(opacity.value === 1 ? 0 : 1, { stiffness: 10, damping: 2, velocity: 2 });
+    scale.value = withSpring(scale.value === 1 ? 1.5 : 1);
+    rotate.value = withSpring(rotate.value === 0 ? 45 : 0);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Animated.View style={[styles.animate, animatedStyle]} />
+       <TouchableOpacity style={styles.button}  onPress={startAnimation}>
+          <Text style={styles.text}>Start Animation</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  animate: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    backgroundColor: 'red',
+    margin: 10,
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
 });
+
+export default App;
